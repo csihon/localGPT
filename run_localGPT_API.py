59d312e25d50cf6d49bb2929e3c0461b996141be
csihon <csihon@msn.com>
2024-08-13 16:17:45 -0600

    inital commit

--- run_localGPT_API.py
+++ run_localGPT_API.py
@@ -9,7 +9,7 @@ from flask import Flask, jsonify, request
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 
-# from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 from run_localGPT import load_model
 from prompt_template_utils import get_prompt_template
 
